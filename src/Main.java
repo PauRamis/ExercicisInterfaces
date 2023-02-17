8060177949fb0b1e2d250a574d1cd6d9d66234e9
Pau <pauramisnunez@gmail.com>
2023-02-17 16:21:18 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         for (int i = 0; i < vehicles.length; i++) {
             IsVehicle v = vehicles[i];
-            if (v instanceof Jeep){
+            if (v instanceof Jeep) {
 
             }
         }
